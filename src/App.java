d3f3aa8e93730bb53ff1ef6e6a55661176c00c89
PabloMarquez23 <pabloandresmarquezjimenez@gmail.com>
2024-09-25 13:10:52 -0500

    Segundo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Metodos metodosBubble = new Metodos();
+        int[] arreglo = { 5, 7, 30, 12, 9 };
+
+        int[] arregloOrdenado = metodosBubble.sortByBubble(arreglo);
+        metodosBubble.printArreglo(arregloOrdenado);
+
+        int[] arregloOrdenadoDes = metodosBubble.sortByBubbleDES(arreglo);
+        metodosBubble.printArregloDES(arregloOrdenadoDes);
+
+        /////////////////////////
+
+        String cadena  = "este es un ejemplo de texto para ordenar";
+        String[] palabras=cadena.split(" ");
+        String[] palabrasOrdenara = metodosBubble.sortByBubblePalabras(palabras);
+        metodosBubble.printArregloPalabras(palabrasOrdenara);
     }
+
 }
---
+++ src/Metodos.java
@@ -0,0 +1,71 @@
+public class Metodos {
+    /*
+     * public Metodos() {
+     * System.out.println("Se creo la clase metodos");
+     * }
+     */
+
+    public int[] sortByBubble(int[] arreglo) {
+        int tamanio = arreglo.length;
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = i + 1; j < tamanio; j++) {
+                if (arreglo[i] > arreglo[j]) { // El signo ">" es de menor a mayor y el "<" es de mayor a menor
+                    int aux = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = aux;
+                }
+            }
+        }
+        return arreglo;
+    }
+
+    public void printArreglo(int[] arreglo) {
+        for (int elemento : arreglo) {
+            System.out.print(elemento+" ");
+        }
+    }
+
+    ////////////////////////////////////////
+    public int[] sortByBubbleDES(int[] arreglo) {
+        int tamanio = arreglo.length;
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = i + 1; j < tamanio; j++) {
+                if (arreglo[i] < arreglo[j]) { // El signo ">" es de menor a mayor y el "<" es de mayor a menor
+                    int aux = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = aux;
+
+                }
+            }
+        }
+        return arreglo;
+    }
+
+    public void printArregloDES(int[] arreglo) {
+        for (int elemento : arreglo) {
+            System.out.print(elemento+" ");
+        }
+    }
+
+    //////////////////////////////////////////
+    public String[] sortByBubblePalabras(String[] palabras) {
+        int tamanio = palabras.length;
+
+        for (int i = 0; i < tamanio; i++) {
+            for (int j = i + 1; j < tamanio; j++) {
+                if (palabras[i].compareToIgnoreCase(palabras[j]) > 0) {
+                    String aux = palabras[i];
+                    palabras[i] = palabras[j];
+                    palabras[j] = aux;
+                }
+            }
+        }
+        return palabras;
+    }
+
+        public void printArregloPalabras(int[] arreglo) {
+        for (int elemento : arreglo) {
+            System.out.print(elemento+" ");
+        }
+    }
+}
